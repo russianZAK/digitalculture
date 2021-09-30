01185a2807882784dcaaa4f8fadd916a7e5b447e
Nikita Fisenko <nikitafisenko717@gmail.com>
2021-09-30 19:39:07 +0300

    aded whaaaaaaat.c

--- new_prog.c
+++ new_prog.c
@@ -1,7 +1,3 @@
-#include <stdio.h> 
-
-int main()
-{
-    printf("Hello Nikita!");
-    return 0;
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:b950196b832d410e76e295adb453081b2487bb2b828b520caf32f3ee4294f2f9
+size 84
---
+++ whhhaaaat.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f24233f3d35fa6f3155da86b47f4974d681693bd2293ec6ccc35942a3b7bd224
+size 3257
